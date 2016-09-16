b40814ed354b300fc07dafef5ac088abf1a47639
Maryada Shrestha <mshrestha83@i21a-s2.ufhpc>
2016-09-16 12:08:16 -0400

    Converting to yesterday's date

--- weather.sh
+++ weather.sh
@@ -1,25 +1,13 @@
 #!/bin/bash
 # test
 
-echo "What do you want?"
+year=`date -d yesterday +%Y`
 
-read dat
+month=`date -d yesterday +%m`
 
-echo "What month do you want?"
+day=`date -d yesterday +%d`
 
-read month
-
-echo "What year do you want?"
-
-read year
-
-year=date -d yesterday +%Y
-
-month= date -d yesterday +%M
-
-day= date -d yesterday +%D
-
-curl "https://www.wunderground.com/history/airport/GNV/$year/$month/$day/DailyHistory.heml?&format=1" > gnv.txt
+curl "https://www.wunderground.com/history/airport/GNV/$year/$month/$day/DailyHistory.html?&format=1" > gnv.txt
 
 maxTemp=`awk -F',' '{print $2}' gnv.txt | sort -n | tail -n1`
 
