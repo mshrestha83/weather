6ba1b88190269c5a2fff3719bae6265620771734
Maryada Shrestha <mshrestha83@i21a-s2.ufhpc>
2016-09-16 09:56:53 -0400

    Change to use yesterday's date for weather

--- weather.sh
+++ weather.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl "https://www.wunderground.com/history/airport/GNV/2016/09/06/DailyHistory.heml?&format=1" > gnv.txt
+curl "https://www.wunderground.com/history/airport/GNV/2016/09/15/DailyHistory.heml?&format=1" > gnv.txt
 
 maxTemp=`awk -F',' '{print $2}' gnv.txt | sort -n | tail -n1`
 
